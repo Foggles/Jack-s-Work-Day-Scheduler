6d52df1378880be0e60e52147f49bd92972df5a8
Foggles <jackfogarty2105@gmail.com>
2020-12-10 23:27:13 +0800

    Added data attributes to each row to store their time value.

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-let currentDayEl = document.getElementById("currentDay")
 let timeRowContainer = document.getElementById("timeRowContainer");
+let currentTime = moment().format("ha");
+let newTimeRow;
+console.log(currentTime)
+
 
 // load time into the dom
-
 setInterval(function () {
     $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 
@@ -12,38 +14,44 @@ setInterval(function () {
 // set up table
 // shouldn't create the timetable via raw html
 
-const times = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
+// Array containing all times to be used
+const times = ["7am", "8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm", "8pm", "9pm", "10pm", "11pm"];
 
-// for loop 
+// for loop to build the time rows
 for (let index = 0; index < times.length; index++) {
     const time = times[index];
 
     // in each loop we create a time row
-    let newTimeRow = document.createElement("div");
-    newTimeRow.classList.add("row");
+    newTimeRow = document.createElement("div");
+    newTimeRow.classList.add("row",);
 
     // each row would have the time
     let newTimeCol = document.createElement("div");
-    newTimeCol.classList.add("col");
+    newTimeCol.classList.add("col-2", "hour");
     newTimeCol.textContent = times[index];
 
     // textarea
     let newTextAreaCol = document.createElement("div");
     let newTextArea = document.createElement("textarea");
+    newTextArea.rows = "4";
+    newTextArea.cols = "65";
 
-    newTextAreaCol.classList.add("col");
+    newTextAreaCol.classList.add("col-8", "description");
     newTextAreaCol.appendChild(newTextArea);
 
     // button
     let newButtonCol = document.createElement("div");
     let newButton = document.createElement("button");
+    newButton.textContent = "TESTING"
 
-    newButtonCol.classList.add("col");
+    newButtonCol.classList.add("col-2", "saveBtn");
     newButtonCol.appendChild(newButton);
 
+    // Appending all the new elements to the new row
     newTimeRow.appendChild(newTimeCol);
     newTimeRow.appendChild(newTextAreaCol);
     newTimeRow.appendChild(newButtonCol);
+    newTimeRow.setAttribute("time", times[index]);
 
 
 
@@ -62,7 +70,6 @@ for (let index = 0; index < times.length; index++) {
 
         // i want to get the value of textarea
         // data = row.secondChild().firstChild().val()
-        // TODO: 
 
         // FIXME: 
         localStorage.setItem('data-row-' + time, data)
@@ -77,5 +84,18 @@ for (let index = 0; index < times.length; index++) {
 
 
 
+    // Appending it all to the container
+    timeRowContainer.classList.add("time-block")
     timeRowContainer.appendChild(newTimeRow);
-}
+};
+
+// TODO: Use moment.js to change the colour of the timeRowContainer depending on the time
+// TODO: (Past, Present & Future)
+// if (newTimeRow.getAttribute("time") === currentTime) {
+//     // let trueTimeRow = newTimeRow.getAttribute("time") === currentTime;
+//     newTimeRow.classList.add("present");
+// };
+
+// PLAN: Use something similar to "$("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));"" and add it to each timeRow,
+// PLAN: Have the array use this ^ instead of strings
+// PLAN: Then compare the time in the timeRow to the current time and use that to change the colour of the timeRow accordingly.
