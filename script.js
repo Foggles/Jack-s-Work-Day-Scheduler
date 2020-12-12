10b9b349cb682ba5b7b068a9af4c0e6aa89389b1
Foggles <jackfogarty2105@gmail.com>
2020-12-12 13:54:02 +0800

    Added functionality to the save button to grab and store any text within the text area in local storage

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ let timeRowContainer = document.getElementById("timeRowContainer");
 let currentTime = moment().format("kk:00");
 let currentTimeNumber = parseInt(currentTime);
 let newTimeRow;
-console.log(currentTimeNumber)
+console.log(currentTimeNumber + ":00")
 
 
-// load time into the dom
+// loading current time into the DOM
 setInterval(function () {
     $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 
@@ -15,45 +15,48 @@ setInterval(function () {
 // set up table
 // shouldn't create the timetable via raw html
 
-// Array containing all times to be used
-// const times = ["7:00", "8:00", "9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"];
-
-// const times = ["07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"];
-
+// Array containing all times to be used, utilising moment.js library
 const times = [moment().hour(7).minute(0).format("kk:00"), moment().hour(8).minute(0).format("kk:00"), moment().hour(9).minute(0).format("kk:00"), moment().hour(10).minute(0).format("kk:00"), moment().hour(11).minute(0).format("kk:00"), moment().hour(12).minute(0).format("kk:mm"), moment().hour(13).minute(0).format("kk:mm"), moment().hour(14).minute(0).format("kk:mm"), moment().hour(15).minute(0).format("kk:mm"), moment().hour(16).minute(0).format("kk:mm"), moment().hour(17).minute(0).format("kk:mm"), moment().hour(18).minute(0).format("kk:mm"), moment().hour(19).minute(0).format("kk:mm"), moment().hour(20).minute(0).format("kk:mm"), moment().hour(21).minute(0).format("kk:mm"), moment().hour(22).minute(0).format("kk:mm"), moment().hour(23).minute(0).format("kk:mm")];
 
 // for loop to build the time rows
 for (let index = 0; index < times.length; index++) {
     const time = times[index];
 
-    
+
     console.log(times[index])
 
     // in each loop we create a time row
     newTimeRow = document.createElement("div");
     newTimeRow.classList.add("row",);
+    newTimeRow.id = "timeRow";
 
     // each row would have the time
     let newTimeCol = document.createElement("div");
     newTimeCol.classList.add("col-2", "hour");
     newTimeCol.textContent = times[index];
+    newTimeCol.id = "timeCol";
 
     // textarea
     let newTextAreaCol = document.createElement("div");
     let newTextArea = document.createElement("textarea");
     newTextArea.rows = "4";
     newTextArea.cols = "65";
+    newTextArea.id = "textArea";
 
     newTextAreaCol.classList.add("col-8", "description");
     newTextAreaCol.appendChild(newTextArea);
+    newTextAreaCol.id = "textAreaContainer";
 
     // button
     let newButtonCol = document.createElement("div");
     let newButton = document.createElement("button");
-    newButton.textContent = "TESTING"
+    newButton.innerHTML = "<i class='far fa-save fa-3x'></i>";
+    newButton.classList.add("saveBtn");
+    newButton.id = "saveButton";
 
-    newButtonCol.classList.add("col-2", "saveBtn");
+    newButtonCol.classList.add("col-2", "saveBtnCol");
     newButtonCol.appendChild(newButton);
+    newButtonCol.id = "saveButtonContainer";
 
     let dataTime = parseInt(times[index]);
 
@@ -65,56 +68,51 @@ for (let index = 0; index < times.length; index++) {
 
 
 
-    // attacj event listner
+    // attach event listner to detect when save button is clicked
     $(newButton).on('click', function (event) {
         event.preventDefault();
 
         // identify which row
-
-        $(event.target).parent()
-
+        let target = $(this).parents(document.getElementById("timeRow"));
+        let trueTarget = target[1];
 
         // row
-        // const row;
-        // const time = row.firstChild().text()
+        const childRow = trueTarget;
+        let childRowText = childRow.querySelector("textarea");
+        
+        const time = childRow.outerText;
+
+        console.log(childRow);
+        console.log("Time is - " + time);
+
+
 
         // i want to get the value of textarea
-        // data = row.secondChild().firstChild().val()
+        let data = childRowText.value;
+        console.log("Text to save:" + data);
 
         // FIXME: 
-        localStorage.setItem('data-row-' + time, data)
+        localStorage.setItem("timeRowData" + time, data)
 
     });
-
-
-    // create a data attr on the row
-    // data-row-time 
-    // so we can identify each row
-    // and create a unique key in local storage
-
 
 
     // Appending it all to the container
     timeRowContainer.classList.add("time-block")
     timeRowContainer.appendChild(newTimeRow);
 
+
+    // Assigning each row created a colour depending on the current time
     if (newTimeRow.getAttribute("time") > currentTimeNumber) {
         newTimeRow.classList.add("future");
         newTimeRow.setAttribute("timeColour:", "future")
-    } 
+    }
     if (newTimeRow.getAttribute("time") < currentTimeNumber) {
         newTimeRow.classList.add("past");
         newTimeRow.setAttribute("timeColour:", "past")
-    } 
+    }
     else {
         newTimeRow.classList.add("present");
         newTimeRow.setAttribute("timeColour:", "present");
     }
 };
-
-// TODO: Use moment.js to change the colour of the timeRowContainer depending on the time
-// TODO: (Past, Present & Future)
-
-// PLAN: Use something similar to "$("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));"" and add it to each timeRow,
-// PLAN: Have the array use this ^ instead of strings
-// PLAN: Then compare the time in the timeRow to the current time and use that to change the colour of the timeRow accordingly.
